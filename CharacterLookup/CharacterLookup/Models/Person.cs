ea46ec342e4143b0d3a4251937161a5bced764b3
Pete Heiss (Yoh Services LLC) <v-peheis@microsoft.com>
2020-08-24 12:10:55 -0700

    Uploading files from weekend work.

---
+++ CharacterLookup/CharacterLookup/Models/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CharacterLookup.Models
+{
+    public class Person
+    {
+
+    }
+}
