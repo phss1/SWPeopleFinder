b4d845f8691a9ee827b788a7521b9df05a5c3b10
Pete Heiss (Yoh Services LLC) <v-peheis@microsoft.com>
2020-08-27 21:13:34 -0700

    Saving changes 08/27 2113

--- PersonLookup/PersonLookup/Pages/Results.cshtml.cs
+++ PersonLookup/PersonLookup/Pages/Results.cshtml.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using PersonLookup.Models;
+using PersonLookup.Models.Specie;
 using PersonLookup.Tools;
 
 namespace PersonLookup.Pages
@@ -13,16 +14,37 @@ namespace PersonLookup.Pages
         [BindProperty(SupportsGet = true)]
         public string value { get; set; }
         public IEnumerable<People> People { get; set; }
+        private IEnumerable<People> newPeopleLst;
+        private IEnumerable<Specie> _specie;
 
         public ResultsModel(IPersonRepository personRepo)
         {
             personRepository = personRepo;
         }
 
-         public void OnGet(string value)
+        public ResultsModel(ISpecieRepository specieRepo)
         {
-            //IEnumerable<People> results = (personRepository.FindPerson(value)).;
-            People = personRepository.FindPerson(value);
+            //_specie = specieRepo;
         }
+
+        public void OnGet(string value)
+        {
+            newPeopleLst = personRepository.FindPerson(value);
+
+            //People = GetPersonSpecies((List<People>)newPeopleLst);
+        }
+
+        //private List<People> GetPersonSpecies(List<People> list)
+        //{
+        //    List<People> newPeopleList;
+
+        //    foreach(People person in list)
+        //    {
+        //        string speciesUrl = person.species.ToString();
+
+        //    }
+
+        //    return newPeopleList;
+        //}
     }
 }
